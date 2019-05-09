057e48b63e2837e44412763a3f90f9babdda526e
BuildTools <jromphf@library.rochester.edu>
2019-05-08 23:07:54 -0400

    working on osx

--- conanfile.py
+++ conanfile.py
@@ -14,20 +14,22 @@ class OpenvdbpointsunityConan(ConanFile):
     generators = "cmake"
 
     def source(self):
-        # eventually pull from github
-        tools.replace_in_file("{}/openvdb-points-unity-{}/CMakeLists.txt".format(self.source_folder, self.version), "PROJECT(openvdb-points-unity)",
+        # TODO deal with versioning later
+        self.run("git clone https://github.com/rochester-rcl/openvdb-points-unity.git openvdb-points-unity")
+        tools.replace_in_file("{}/openvdb-points-unity/CMakeLists.txt".format(self.source_folder), "PROJECT(openvdb-points-unity)",
                               '''PROJECT(openvdb-points-unity)
 include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
 conan_basic_setup()''')
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_folder="{}/openvdb-points-unity-{}".format(self.source_folder, self.version))
+        cmake.configure(source_folder="{}/openvdb-points-unity".format(self.source_folder))
         cmake.build()
 
+    # TODO will make this more organized once prototyping is done
     def package(self):
-        self.copy("*.h", dst="include", src="hello")
-        self.copy("*hello.lib", dst="lib", keep_path=False)
+        self.copy("*.h", dst="include", src="{}/openvdb-points-unity/src".format(self.source_folder))
+        self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
